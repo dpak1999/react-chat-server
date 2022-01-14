7de762e8fc62345420bd502c2d26268747f7e662
deepak <deepakkumardash313@gmail.com>
2022-01-14 22:13:49 +0530

    add : twilio message sending

--- index.js
+++ index.js
@@ -10,11 +10,40 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twilioClient = require('twilio')(accountSid, authToken);
+
 // routes
 const authroutes = require('./routes/auth');
 
 app.get('/', (req, res) => {
   res.send('Hello world');
+});
+
+app.post('/', (req, res) => {
+  const { message, user: sender, type, members } = req.body;
+
+  if (type === 'message.new') {
+    members
+      .filter((member) => member.user._id !== sender.id)
+      .forEach(({ user }) => {
+        if (!user.online) {
+          twilioClient.messages
+            .create({
+              body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+              messagingServiceSid: messagingServiceSid,
+              to: user.phoneNumber,
+            })
+            .then(() => console.log('Message sent'))
+            .catch((err) => console.log(err));
+        }
+      });
+    return res.status(200).send('Message sent');
+  }
+
+  return res.status(200).send('Not a new Message');
 });
 
 app.use('/auth', authroutes);
